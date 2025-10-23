d285481c2cc37de938f0fdd7071b799647020d25
bisaraid <fazafa.id@gmail.com>
2025-10-23 18:10:39 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,8 @@
-
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-xl font-bold mb-4">Bisara POS</h1>
+      <p>Silakan pilih modul: Produk, Transaksi, Promo, Kasbon, Shift, Insight</p>
+    </main>
+  )
+}
