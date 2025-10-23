e66c9472b1657d40087735a022427b8256702a8a
bisaraid <fazafa.id@gmail.com>
2025-10-23 18:12:25 +0700

    Update route.ts

--- app/api/kasbon/route.ts
+++ app/api/kasbon/route.ts
@@ -1,1 +1,6 @@
+import { supabase } from '@/lib/supabaseClient'
 
+export async function GET() {
+  const { data, error } = await supabase.from('kasbon').select('*')
+  return Response.json(error ? { error } : data)
+}
