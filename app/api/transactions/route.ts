15c6c83dc5e8c83b776d315ab79af0d756fbd285
bisaraid <fazafa.id@gmail.com>
2025-10-25 15:48:46 +0700

    Update route.ts

--- app/api/transactions/route.ts
+++ app/api/transactions/route.ts
@@ -1,4 +1,4 @@
-import { supabase } from '@/lib/supabaseClient'
+import { supabase } from '../../../lib/supabaseClient'
 
 export async function GET() {
   const { data, error } = await supabase.from('transactions').select('*')
